1575c3478ed4115c3aa052c4564ec396b394b25a
fernanda43 <81189772+fernanda432@users.noreply.github.com>
2021-03-22 21:25:03 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDNB5IdpaOiw7iGSfNJrk6AR8bWbiQlxUU",
+    authDomain: "autentificacion43gvlf.firebaseapp.com",
+    projectId: "autentificacion43gvlf",
+    storageBucket: "autentificacion43gvlf.appspot.com",
+    messagingSenderId: "1012902297327",
+    appId: "1:1012902297327:web:e43b8b093fef7c813ca51a",
+    measurementId: "G-PFWFFP9EPJ"
+});
